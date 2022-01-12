c570996d00fc0c514c9c613d9eb35b6e0c8245ad
0xOmarA <OmarAbdulla7@hotmail.com>
2022-01-13 00:20:52 +0300

    Fixed some issues in the quick transaction in the Wallet class

--- src/Radix/__init__.py
+++ src/Radix/__init__.py
@@ -3,5 +3,6 @@ from .provider import Provider
 from .network import Network
 from .signer import Signer
 from .wallet import Wallet
+
 from . import network_specific_constants as NetworkSpecificConstants
 from . import utils as utils
