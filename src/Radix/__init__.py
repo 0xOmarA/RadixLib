d6f5ea2e8c9eb4711528e6fd253c1bb5002b8ba8
0xOmarA <OmarAbdulla7@hotmail.com>
2022-01-13 09:25:36 +0300

    Created the token class

--- src/Radix/__init__.py
+++ src/Radix/__init__.py
@@ -3,6 +3,7 @@ from .provider import Provider
 from .network import Network
 from .signer import Signer
 from .wallet import Wallet
+from .token import Token
 
 from . import network_specific_constants as NetworkSpecificConstants
 from . import utils as utils
