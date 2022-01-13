b25d1bae053677e58c439ce0dd1bd1c82a2bbe35
0xOmarA <OmarAbdulla7@hotmail.com>
2022-01-13 10:02:16 +0300

    Updated the minting example to use the new wallet

--- examples/minting a mutable token/main.py
+++ examples/minting a mutable token/main.py
@@ -19,43 +19,28 @@ def main() -> None:
     mnemonic_phrase: str = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon"
     network: Radix.Network = Radix.Network.STOKENET
 
-    # Creating a new proider and signer to use to talk to the Radix API
-    provider: Radix.Provider = Radix.Provider(network = network)
-    signer: Radix.Signer = Radix.Signer.from_mnemonic(mnemonic_phrase)
-    wallet_address: str = signer.wallet_address(index = 0, mainnet = True if network is Radix.Network.MAINNET else False)
+    # Creating a new wallet object with a provider and a signer to use in 
+    # this example
+    wallet: Radix.Wallet = Radix.Wallet(
+        provider =  Radix.Provider(network = network),
+        signer = Radix.Signer.from_mnemonic(mnemonic_phrase),
+        index = 0,
+    )
 
-    # Creating the transaction for the minting of the token
-    response: dict = provider.build_transaction(
+    # Perform the transaction through the wallet object and get the transaction
+    # id after it is done.
+    tx_id: str = wallet.build_sign_and_send_transaction(
         actions = Radix.Action.new_token_mint_action(
             to_address = to_address,
             amount = amount,
             rri = token_rri
         ),
-        fee_payer = wallet_address,
-        message = "0000" + "I have entrust you with 100 of my test tokens. Use them wisely...".encode('utf-8').hex()
-    ).json()
+        # message = "Speak to you soon!",
+        message = "Speak to you soon my friend.",
+        encrypt_message = True,
+        encrypt_for_address = to_address
+    )
 
-    if 'error' in response.keys():
-        raise Exception(f"An error has occured when building the transaction: {response['error']}")
-
-    # Getting the blob which needs to be signed and signing it using the signer object
-    blob: str = response['result']['transaction']['blob']
-    hash_of_blob_to_sign: str = response['result']['transaction']['hashOfBlobToSign']
-    signed_data: str = signer.sign(hash_of_blob_to_sign)
-
-    # Submit the transaction to the blockchain, thus creating the token 
-    response: dict = provider.finalize_transaction(
-        blob = blob,
-        signature_der = signed_data,
-        public_key_of_signer = signer.public_key(),
-        immediateSubmit = True
-    ).json()
-
-    if 'error' in response.keys():
-        raise Exception(f"An error has occured when finalizing the transaction: {response['error']}")
-
-    print(response)
-    tx_id: str = response['result']['txID']
     print("Token have been minted and sent to your recipient under transaction:", tx_id)
 
 if __name__ == "__main__":
