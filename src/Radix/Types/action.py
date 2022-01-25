0fbb4db822ddf3acec8aaccf26ee02fad18e3de6
0xOmarA <OmarAbdulla7@hotmail.com>
2022-01-25 20:17:57 +0300

    Formatting changes

--- src/Radix/Types/action.py
+++ src/Radix/Types/action.py
@@ -4,10 +4,7 @@ import json
 
 class Action():
     """ 
-    A class which acts as a wrapper for Radix Actions. Built with references to the 
-    following GitHub pages:
-    * https://github.com/radixdlt/radixdlt/tree/1.0.6/radixdlt-java/radixdlt-java/src/main/java/com/radixdlt/client/lib/api/action
-    * https://github.com/radixdlt/radixdlt/blob/5f63817be7a2f3dda54c3fa12a29d041f7b58ff9/radixdlt-java/radixdlt-java/src/main/java/com/radixdlt/client/lib/api/ActionType.java
+    A class which acts as a wrapper for Radix Actions.
     """
 
     def __init__(
@@ -17,10 +14,14 @@ class Action():
     ) -> None:
         """ 
         Creates a new action object with the keyword arguments provided. 
+        
         # Arguments
+
         * `kwargs: dict` - A dictionary of keyword arguments which will make up the final
         action dict.
+        
         # Raises
+        
         * `ValueError` - If the `type` argument passed is neither an `ActionType` or a stirng
         """
 
@@ -72,13 +73,17 @@ class Action():
     ) -> 'Action':
         """ 
         Creates a new action of the type "TokenTransfer"
+        
         # Arguments
+        
         * `from_address: str` - A string of the address which will be sending the tokens.
         * `to_address: str` - A string of the address which will be getting the tokens.
         * `amount: int` - An integer of the amount of tokens to send. As an example, if we want to
         send 10 XRD we would specify an amount of 10000000000000000000.
         * `rri: str` - The Radix Resource Identifier (RRI) of the token which we will be sending.
+        
         # Returns
+        
         * `Action` - An action object of this token transfer action.
         """
 
@@ -99,12 +104,16 @@ class Action():
     ) -> 'Action':
         """
         Creates a new action of the type "StakeTokens"
+        
         # Arguments
+        
         * `from_address: str` - A string of the address which will be staking their tokens.
         * `validator: str` - A string of the validator's address which the tokens will be staked to.
         * `amount: str` - An integer of the amount of XRD to be staked. As an example, if I wish to stake
         10 XRD, then the amount would be 10000000000000000000.
+        
         # Returns
+        
         * `Action` - An action object of this XRD staking.
         """
 
@@ -124,12 +133,16 @@ class Action():
     ) -> 'Action':
         """
         Creates a new action of the type "UnstakeTokens"
+        
         # Arguments
+        
         * `from_address: str` - A string of the address which will be unstaking their tokens.
         * `validator: str` - A string of the validator's address which the tokens will be unstaked from.
         * `amount: str` - An integer of the amount of XRD to be unstaked. As an example, if I wish to unstake
         10 XRD, then the amount would be 10000000000000000000.
+        
         # Returns
+        
         * `Action` - An action object of this XRD unstaking.
         """
 
@@ -149,11 +162,15 @@ class Action():
     ) -> 'Action':
         """
         Creates a new action of the type "BurnTokens"
+        
         # Arguments
+        
         * `from_address: str` - A string of the address which we wish to burn the tokens from.
         * `amount: int` - An integer of the amount of tokens which we wish to burn.
         * `rri: str` - A string of the Radix Resource Identifier (RRI) of the token which we will be burning. 
+        
         # Returns
+        
         * `Action` - An action object of this burn.
         """
 
@@ -173,11 +190,15 @@ class Action():
     ) -> 'Action':
         """
         Creates a new action of the type "MintTokens"
+        
         # Arguments
+        
         * `to_address: str` - A string of the address which we wish to mint the tokens to.
         * `amount: int` - An integer of the amount of tokens which we wish to mint.
         * `rri: str` - A string of the Radix Resource Identifier (RRI) of the token which we will be minting. 
+        
         # Returns
+        
         * `Action` - An action object of this burn.
         """
 
@@ -197,11 +218,15 @@ class Action():
     ) -> 'Action':
         """
         Creates a new action of the type "RegisterValidator"
+        
         # Arguments
+        
         * `validator: str` - The address of the validator which we wish to register.
         * `name: str` - The name of the validator.
         * `url: str` - The URL to use to query this validator.
+        
         # Returns
+        
         * `Action` - An action object of this validator registration.
         """
 
@@ -221,11 +246,15 @@ class Action():
     ) -> 'Action':
         """
         Creates a new action of the type "UnregisterValidator"
+        
         # Arguments
+        
         * `validator: str` - The address of the validator which we wish to unregister.
         * `name: str` - The name of the validator.
         * `url: str` - The URL to use to query this validator.
+        
         # Returns
+        
         * `Action` - An action object of this validator unregistration.
         """
 
@@ -245,11 +274,15 @@ class Action():
     ) -> 'Action':
         """
         Creates a new action of the type "UpdateValidatorMetadata"
+       
         # Arguments
+        
         * `validator: str` - The address of the validator which we wish to update.
         * `name: str` - The name of the validator.
         * `url: str` - The URL to use to query this validator.
+        
         # Returns
+
         * `Action` - An action object of this validator metadata update.
         """
 
@@ -268,10 +301,14 @@ class Action():
     ) -> 'Action':
         """
         Creates a new action of the type "UpdateValidatorFee"
+        
         # Arguments
+        
         * `validator: str` - The address of the validator which we wish to update the fees for.
         * `validator_fee: int` - The fee which is asociated with this validator
+        
         # Returns
+        
         * `Action` - An action object of this validator fee update.
         """
 
@@ -289,10 +326,14 @@ class Action():
     ) -> 'Action':
         """
         Creates a new action of the type "UpdateValidatorOwner"
+        
         # Arguments
+        
         * `validator: str` - A string of the validator's address which we wish to update the owner for.
         * `to_address: str` - A string of the address which we want to transfer the ownership to.
+        
         # Returns
+        
         * `Action` - An action object of this validator owner update
         """
 
@@ -310,10 +351,14 @@ class Action():
     ) -> 'Action':
         """
         Creates a new action of the type "UpdateAllowDelegationFlag"
+        
         # Arguments
+        
         * `validator: str` - A string of the validator address that we wish to update the delegation flag for.
         * `allow_delegation: bool` - A boolean of whether delegation to this pool is allowed or not.
+        
         # Returns
+        
         * `Action` - An action object of this validator delegation flag update.
         """
 
@@ -337,7 +382,9 @@ class Action():
     ) -> 'Action':
         """
         Creates a new action of the type "CreateFixedSupplyToken"
+        
         # Arguments
+        
         * `to_address: str` - A string of the address which the tokens will be created and minted
         to.
         * `public_key_of_signer: str` - A string of the public key of the signer. In most of the 
@@ -348,7 +395,9 @@ class Action():
         * `icon_url: str` - A string of the URL of the token icon.
         * `token_url`: str - A string of the URL for the token. Could be the website for the token.
         * `supply: str` - A string of the total supply of the token.
+        
         # Returns
+        
         * `Action` - An action object of this new token
         """
 
@@ -376,7 +425,9 @@ class Action():
     ) -> 'Action':
         """
         Creates a new action of the type "CreateMutableSupplyToken"
+        
         # Arguments
+        
         * `public_key_of_signer: str` - A string of the public key of the signer. In most of the 
         cases, this is the public key of the `to_address` or whoever will sign the transaction.
         * `symbol: str` - A string of the symbol that the token will have.
@@ -384,7 +435,9 @@ class Action():
         * `description: str` - A string of the description of the token.
         * `icon_url: str` - A string of the URL of the token icon.
         * `token_url`: str - A string of the URL for the token. Could be the website for the token.
+        
         # Returns
+        
         * `Action` - An action object of this new token
         """
         return cls(
