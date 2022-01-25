c3912c6876508ce7b188fa1019bb23383a840798
0xOmarA <OmarAbdulla7@hotmail.com>
2022-01-25 20:22:55 +0300

    Added the camel_case_to_snake_case function

--- src/Radix/utils.py
+++ src/Radix/utils.py
@@ -1,3 +1,6 @@
+import re
+
+
 def remove_none_values_recursively(dictionary: dict) -> dict:
     """
     Recursively removes the key value pairs where the value is None. 
@@ -16,3 +19,20 @@ def remove_none_values_recursively(dictionary: dict) -> dict:
         for key, value in dictionary.items()
         if value is not None
     }
+
+
+def camel_case_to_snake_case(string: str) -> str:
+    """
+    This method is used to transform a camel_case_string into snake case.
+    
+    # Arguments
+    
+    * `string: str` - A string in camelCase which we want to convert to snake_case.
+    
+    # Returns
+    
+    * `str` - The string in snake_case.
+    """
+
+    string: str = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', string)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', string).lower()
