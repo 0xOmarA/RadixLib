005814ebbd59a6413cb895c9410ba2bda412e37d
0xOmarA <OmarAbdulla7@hotmail.com>
2022-01-25 20:49:04 +0300

    minor change

--- src/Radix/utils.py
+++ src/Radix/utils.py
@@ -34,5 +34,6 @@ def camel_case_to_snake_case(string: str) -> str:
     * `str` - The string in snake_case.
     """
 
-    string: str = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', string)
-    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', string).lower()
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', 
+        re.sub('(.)([A-Z][a-z]+)', r'\1_\2', string)
+    ).lower()
