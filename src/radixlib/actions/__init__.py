41aa6e924ebdb0101a003a1d84d48b4618bc5f98
0xOmarA <OmarAbdulla7@hotmail.com>
2022-01-29 23:45:46 +0300

    Basic implementation of the actions

--- src/radixlib/actions/__init__.py
+++ src/radixlib/actions/__init__.py
@@ -1,7 +1,8 @@
 # type: ignore
 
 from radixlib.actions.create_token_definition import CreateTokenDefinition
-
-from typing import Union as __Union
-
-ActionType: __Union = __Union[CreateTokenDefinition, None]
+from radixlib.actions.transfer_tokens import TransferTokens
+from radixlib.actions.unstake_tokens import UnstakeTokens
+from radixlib.actions.stake_tokens import StakeTokens
+from radixlib.actions.mint_tokens import MintTokens
+from radixlib.actions.burn_tokens import BurnTokens
