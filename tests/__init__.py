5ab40e91d5d8a97d6f4ac7ab54d2ffafa05f9879
0xOmarA <OmarAbdulla7@hotmail.com>
2022-02-01 14:46:10 +0300

    Minor refactoring

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,6 @@
 # type: ignore
 
-from tests.api_types_tests import TestTokenAmount
+from tests.api_types_tests import *
 
 from tests.test_derive import TestDerive
 from tests.test_signer import TestSigner
