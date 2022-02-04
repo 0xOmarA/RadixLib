fd2151e2caf5b7e6875f3a9ad53f48fab04cf56d
0xOmarA <OmarAbdulla7@hotmail.com>
2022-02-04 09:02:57 +0300

    Added an example for querying for account balances

---
+++ examples/8- query account balance/method 1.py
@@ -0,0 +1,40 @@
+"""
+In this example we demonstrate how you can obtain the balances of a wallet.
+
+This method uses the provider without using a signer as there is no need to create a full wallet 
+object to perform this simple query (you can do it from the wallet object, it's just not needed.)
+"""
+
+from typing import Dict, Any
+import radixlib as radix
+
+def main() -> None:
+    # The address of the account that we want to get the balances for
+    accound_address: str = "tdx1qspqqecwh3tgsgz92l4d4f0e4egmfe86049dj75pgq347fkkfmg84pgx9um0v"
+
+    # Defining the network that the token will be created on
+    network: radix.network.Network = radix.network.STOKENET
+
+    # Creating the provider object which is esentially our link or connection to the blockchain
+    # via the gateway API.
+    provider: radix.Provider = radix.Provider(network)
+
+    # Getting the token information and printing it to the console
+    account_balances: Dict[str, Any] = provider.get_account_balances(accound_address)
+    print("Raw balances info:", account_balances, '\n\n')
+
+    # ###############################################
+    # ---------- Optional Response Parsing ----------
+    # ###############################################
+    # At this point, you have already obtained information about the balances and have printed it to 
+    # the console. RadixLib offers a parser which helps parse responses from the gateway API into
+    # an easy to query format. You can use this parsed format if you prefer or if you prefer using
+    # the original format obtained by the API then feel free to do that.
+    parsed_account_balances: Dict[str, Any] = radix.parsers.DefaultParser.parse(
+        data = account_balances,
+        data_type = 'get_account_balances'
+    )
+    print("Parsed balances info:", parsed_account_balances)
+
+if __name__ == "__main__":
+    main()
---
+++ examples/8- query account balance/method 2.py
@@ -0,0 +1,33 @@
+"""
+This example shows how you can use the wallet object to get the balances for the currently loaded
+account in the wallet.
+"""
+
+from typing import Dict, Any
+import radixlib as radix
+import os
+
+def main() -> None:
+    # Defining the network that the token will be created on
+    network: radix.network.Network = radix.network.STOKENET
+
+    # Getting the mnemonic phrase for the wallet that we will be connecting to and using to create
+    # the token. In this case, my mnemonic phrase is stored in an envirnoment variable under the 
+    # name "MNEMONIC_PHRASE". You might want to do the same or you could also just put your mnemonic
+    # phrase as a literal string. 
+    mnemonic_phrase: str = os.environ['MNEMONIC_PHRASE']
+
+    # Creating a new wallet object using the mnemonic phrase above on the network defined.
+    wallet: radix.Wallet = radix.Wallet(
+        provider = radix.Provider(network),
+        signer = radix.Signer.from_mnemonic(mnemonic_phrase)
+    )
+    print("Wallet address:", wallet.address)
+    print("Wallet public key:", wallet.public_key)
+
+    # Getting the balances information and printing it to the console
+    parsed_account_balances: Dict[str, Any] = wallet.get_account_balances()
+    print("Parsed token info:", parsed_account_balances)
+
+if __name__ == "__main__":
+    main()
