960cf32d5544a683774390eec309676ef4c6281f
0xOmarA <OmarAbdulla7@hotmail.com>
2022-02-20 17:26:31 +0300

    Updated the version of the package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="radixlib",
-    version="1.0.0",
+    version="1.1.0",
     author="Omar Abdulla",
     author_email="OmarAbdulla7@hotmail.com",
     description="A Python API wrapper for the Gateway API of the Radix Blockchain.",
